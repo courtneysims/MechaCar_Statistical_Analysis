d62942b18cbd8c49a06d3ba8812cfe75c6971b6b
ca-sims <csims6249@gmail.com>
2022-07-02 17:04:22 -0400

    Deliv 2 create summary stats dataFrames and add resource images.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,5 @@
 # Deliverable 1
+library(tidyverse)
 library(dplyr)
 # Import and read MechaCar_mpg.csv
 MechaCar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -9,4 +10,12 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 
 # Deliverable 2 
+# Import and read MechaCar_mpg.csv
+Suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+# create summary table to get summary stats
+total_summary <- Suspension_coil_table %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STDEV_PSI=sd(PSI))
+# create lot_summary to group each manufacture by summary stats
+lot_summary <- Suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), STDEV_PSI=sd(PSI), .groups = 'keep')
 
+
+#Deliverable 3
